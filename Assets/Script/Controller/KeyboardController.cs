28cda03c914f9f9ce5d10569523b1ea3f403973f
Jean-LoupMARTIN <jlmc78@outlook.fr>
2022-11-02 19:45:38 +0100

    keyboard controller

--- Assets/Script/Controller/KeyboardController.cs
+++ Assets/Script/Controller/KeyboardController.cs
@@ -3,17 +3,17 @@ using UnityEngine;
 
 public class KeyboardController : Controller
 {
-    [SerializeField] KeyCode stickL_left = KeyCode.Q;
-    [SerializeField] KeyCode stickL_right = KeyCode.D;
-    [SerializeField] KeyCode stickL_down = KeyCode.S;
-    [SerializeField] KeyCode stickL_up = KeyCode.Z;
+    [SerializeField] KeyCode key_stickL_left  = KeyCode.Q;
+    [SerializeField] KeyCode key_stickL_right = KeyCode.D;
+    [SerializeField] KeyCode key_stickL_down  = KeyCode.S;
+    [SerializeField] KeyCode key_stickL_up    = KeyCode.Z;
 
-    [SerializeField] KeyCode stickR_left = KeyCode.LeftArrow;
-    [SerializeField] KeyCode stickR_right = KeyCode.RightArrow;
-    [SerializeField] KeyCode stickR_down = KeyCode.DownArrow;
-    [SerializeField] KeyCode stickR_up = KeyCode.UpArrow;
+    [SerializeField] KeyCode key_stickR_left  = KeyCode.LeftArrow;
+    [SerializeField] KeyCode key_stickR_right = KeyCode.RightArrow;
+    [SerializeField] KeyCode key_stickR_down  = KeyCode.DownArrow;
+    [SerializeField] KeyCode key_stickR_up    = KeyCode.UpArrow;
 
-    [SerializeField] KeyCode button1 = KeyCode.Space;
+    [SerializeField] KeyCode key_button1 = KeyCode.Space;
 
 
 
@@ -21,10 +21,10 @@ public class KeyboardController : Controller
     {
         Vector2 stick = Vector2.zero;
 
-        if (Input.GetKey(left)) stick.x -= 1;
+        if (Input.GetKey(left))  stick.x -= 1;
         if (Input.GetKey(right)) stick.x += 1;
-        if (Input.GetKey(down)) stick.y -= 1;
-        if (Input.GetKey(up)) stick.y += 1;
+        if (Input.GetKey(down))  stick.y -= 1;
+        if (Input.GetKey(up))    stick.y += 1;
 
         if (stick != Vector2.zero)
             stick.Normalize();
@@ -32,9 +32,9 @@ public class KeyboardController : Controller
         return stick;
     }
 
-    Vector2 UpdateStickL() => UpdateStick(stickL_left, stickL_right, stickL_down, stickL_up);
-    Vector2 UpdateStickR() => UpdateStick(stickR_left, stickR_right, stickR_down, stickR_up);
+    Vector2 UpdateStickL() => UpdateStick(key_stickL_left, key_stickL_right, key_stickL_down, key_stickL_up);
+    Vector2 UpdateStickR() => UpdateStick(key_stickR_left, key_stickR_right, key_stickR_down, key_stickR_up);
 
     protected override (Vector2, Vector2) UpdateSticks() => (UpdateStickL(), UpdateStickR());
-    protected override bool UpdateButton1() => Input.GetKey(button1);
+    protected override bool UpdateButton1() => Input.GetKey(key_button1);
 }
